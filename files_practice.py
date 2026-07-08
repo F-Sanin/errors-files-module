650eeddb972116acece97af9c64482a563bfa2d2
F-Sanin <fedir.sanin.25@kse.org.ua>
2026-07-08 17:10:01 +0300

    Task 3 completed

--- files_practice.py
+++ files_practice.py
@@ -2,3 +2,12 @@ import csv
 import json
 
 # Ваш код для завдань нижче:
+with open("files/transactions.csv", "r", encoding="UTF-8") as file:
+    transactions = list(csv.DictReader(file))
+transactions_failed = [tr for tr in transactions if tr["status"] == "failed"]
+
+with open("files/transactions_failed.csv", "w", encoding="UTF-8", newline="") as file:
+    columns = list(transactions_failed[0].keys())
+    writer = csv.DictWriter(file, fieldnames=columns)
+    writer.writeheader()
+    writer.writerows(transactions_failed)
